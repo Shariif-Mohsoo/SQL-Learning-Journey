92e84780bdbe99723a69528c0c3ef522520fb80f
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-02 19:14:22 -0700

    Inserting data in table

---
+++ insertingDataInTable.sql
@@ -0,0 +1,8 @@
+
+-- INSERTING THE DATA IN TABLES.
+-- overall insertion
+insert into students values(1,'Anonymous','Hidden');
+-- specific insertion
+insert into students (name,major) values ('Hashim','Computer');
+insert into students (name) values ('Hashim');
+insert into students (major) values ('Biology');
