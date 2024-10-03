4142c7b6fddb01faac75ba330b0bf6cdcfe125da
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-02 19:14:54 -0700

    Updating the records in table

---
+++ deletingRecordsFromTable.sql
@@ -0,0 +1,9 @@
+-- deleting the records
+
+-- deleting the all records
+-- delete from students;
+
+-- deleting the specific record
+delete from students where major = 'undefined';
+delete from students where name = 'undefined';
+select * from students;
