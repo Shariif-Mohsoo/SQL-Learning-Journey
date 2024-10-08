098fb6db913bdd5c7326d662fceccf0a45262513
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-07 17:58:57 -0700

    Creating salgrade table and inserting data

--- quiz3.sql
+++ quiz3.sql
@@ -4,8 +4,8 @@ create table salgrade(
 	HISAL INT
 );
 -- inserting the data;
-insert into salgrade values(1,700,1200);
-insert into salgrade values(2,1201,1400);
+insert into salgrade values(1,700,1280);
+insert into salgrade values(2,1201,1460);
 insert into salgrade values(3,1401,2000);
 insert into salgrade values(4,2001,3000);
 insert into salgrade values(5,3001,9999);
