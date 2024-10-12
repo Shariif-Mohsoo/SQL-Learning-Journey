eaae54774f1e62406f7e63dcc4f4a9741744c11a
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-11 17:47:26 -0700

    Queries with solutions

---
+++ quiz15.sql
@@ -0,0 +1,42 @@
+-- List all the information of emp with Loc and the Grade of all the emps 
+-- belong to the Grade range from 2 to 4 working at the Dept those are not 
+-- starting with char set ‘OP’ and not ending with ‘S’ with the designation
+-- having a char ‘a’ any where joined in the year 1981 but not in the month
+-- of Mar or Sep and Sal not end with ‘00’ in the asc order of Grades 
+select emp.*,dept.loc,salgrade.grade from emp
+join salgrade on emp.sal between salgrade.losal
+and salgrade.hisal
+join dept on emp.deptno = dept.deptno
+where salgrade.grade between 2 and 4
+and dept.dname not like 'OP%'
+and dept.dname not like '%S'
+and emp.job like '%a%'
+and extract(year from emp.hiredate) = 1981
+and extract(month from emp.hiredate) not in(3,9)
+and cast(emp.sal as varchar) not like ('%00')
+order by salgrade.grade asc;
+
+
+
+-- List the details of the Depts along with Empno, Ename or without the emps 
+select dept.*,emp.empno,emp.ename
+from dept left join emp on
+emp.deptno = dept.deptno;
+-- List the details of the emps whose Salaries more than the employee BLAKE
+select * from emp where emp.sal > (
+	select sal from emp where emp.ename = 'BLAKE'
+);
+-- List the emps whose Jobs are same as ALLEN.
+select * from emp where emp.job = (
+	select job from emp where emp.ename = 'ALLEN'
+);
+-- List the emps who are senior to King.
+select * from emp 
+where emp.job in ('MANAGER', 'PRESIDENT') 
+and
+emp.hiredate < 
+(
+	select emp.hiredate from emp where emp.ename = 'KING'
+);
+
+
