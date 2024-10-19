bdd729f65d2e86986926194e74638fd25fff9829
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-18 22:26:12 -0700

    Queries with solution

--- quiz6.sql
+++ quiz6.sql
@@ -1,11 +1,11 @@
--- 5 queries
--- select all the data from emp table.
-select * from emp;
--- select unique jobs from emp table.
-select distinct job from emp;
--- list the employees in assending order of their salaries.
-select * from emp order by sal;
--- list the detail of employees in asc order of deptno and desc order of jobs.
-select * from emp order by deptno asc,job desc;
--- display all the unique job groups in desc order.
-select distinct job from emp order by job desc;
+-- -- 5 queries
+-- -- 1 select all the data from emp table.
+SELECT * FROM EMP;
+-- -- 2 select unique jobs from emp table.
+SELECT DISTINCT EMP.JOB FROM EMP;
+-- -- 3 list the employees in assending order of their salaries.
+SELECT * FROM EMP ORDER BY EMP.SAL  ASC;
+-- -- 4 list the detail of employees in asc order of deptno and desc order of jobs.
+SELECT * FROM EMP ORDER BY EMP.DEPTNO ASC, EMP.JOB DESC;
+-- -- 5 display all the unique job groups in desc order.
+SELECT DISTINCT EMP.JOB FROM EMP ORDER BY EMP.JOB DESC;
