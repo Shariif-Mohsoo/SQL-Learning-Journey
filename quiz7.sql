2168ecb8153bff7f8599f712d3a674a6b4fe3481
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-18 22:26:41 -0700

    Queries with solution

--- quiz7.sql
+++ quiz7.sql
@@ -1,18 +1,18 @@
--- Display all the details of all ‘Mgrs’
-select * from emp where emp.empno in (
-		select distinct mgr from emp
-);
--- . List the emps who joined before 1981
-select * from emp where emp.hiredata < '1981-01-01';
--- List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal
-select empno,ename,sal,(sal/30) as dailySal
-from emp order by (sal * 12);
--- Display the Empno, Ename, job, Hiredate of all Mgrs
-select empno,ename,job,hiredate from emp where emp.empno in
-(
-	select distinct mgr from emp
-);
--- List the Empno, Ename, Sal of all emps working for Mgr 7369. 
-select empno, ename, sal from emp where mgr = 7369;
-
+-- 6 Display all the details of all ‘Mgrs’
+    SELECT * FROM EMP WHERE EMP.EMPNO IN (
+        SELECT DISTINCT EMP.MGR FROM EMP
+    );
+-- 7 List the emps who joined before 1981
+    SELECT * FROM EMP WHERE EMP.HIREDATE <'1981-01-01';
+-- 8 List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal
+    SELECT EMP.EMPNO, EMP.ENAME,EMP.SAL,
+    (EMP.SAL / 30) AS DAILY_SAL 
+    FROM EMP ORDER BY (EMP.SAL * 12) ASC;
+-- 9 Display the Empno, Ename, job, Hiredate of all Mgrs
+    SELECT EMP.EMPNO,EMP.ENAME,EMP.JOB,EMP.HIREDATE FROM 
+    EMP WHERE EMP.MGR IN (
+        SELECT DISTINCT EMP.MGR FROM EMP
+    );
+-- 10 List the Empno, Ename, Sal of all emps working for Mgr 7369. 
+SELECT EMP.EMPNO,EMP.ENAME,EMP.SAL FROM EMP WHERE EMP.MGR = 7369;
 
