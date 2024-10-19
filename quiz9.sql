f001927a2ca66ae9b729e017922a01ec8e136c2d
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-18 22:38:40 -0700

    Queries with solution

--- quiz9.sql
+++ quiz9.sql
@@ -1,24 +1,12 @@
--- List the emp who are working for the Deptno 10 or 20.
-select * from emp where emp.deptno = 10 or emp.deptno = 20;
--- . List the emps who are joined in the year 81
-select * from emp 
-where 
-emp.hiredate > '1981-01-01' 
-and 
-emp.hiredate < '1982-01-01';
- -- List the emps who are joined in the month of Aug 1980
- select * from emp
- where 
- emp.hiredate >= '1980-08-01' 
- and 
- emp.hiredate < '1980-09-01';
- -- List the emps Who Annual sal ranging from 22000 and 45000.
- select * from emp
- where 
- (emp.sal * 12) >= 22000 
- and 
- (emp.sal * 12) <= 45000;
--- List the Enames those are having five characters in their Names
-select * from emp
-where emp.ename like '_____';
-
+-- 16 List the emp who are working for the Deptno 10 or 20.
+SELECT * FROM EMP WHERE EMP.DEPTNO IN (10,20);
+-- 17 List the emps who are joined in the year 81
+SELECT * FROM EMP 
+WHERE EMP.HIREDATE >= '1981-01-01'
+AND EMP.HIREDATE <= '1981-12-31';
+ -- 18 List the emps who are joined in the month of Aug 1980
+SELECT * FROM EMP WHERE EXTRACT(MONTH FROM EMP.HIREDATE) = 8;
+ -- 19 List the emps Who Annual sal ranging from 22000 and 45000.
+SELECT * FROM EMP WHERE (EMP.SAL * 12)    BETWEEN 22000 AND 45000;
+-- 20 List the Enames those are having five characters in their Names
+SELECT EMP.ENAME FROM EMP WHERE EMP.ENAME LIKE '_____';
