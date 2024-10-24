213aabe3d365b168af37a5d71199edb4610f0549
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-23 17:17:57 -0700

    SQL Number Functions

---
+++ quiz21.sql
@@ -0,0 +1,16 @@
+-- THE BELOW WRITTEN ARE NUMBER FUNCTIONS
+-- to random number between 0 and 1
+select random();
+-- to get the square root of number
+select sqrt(16);
+-- to get the number as per given power input
+select power(2, 3);  -- Result: 8 (2 raised to the power of 3)
+-- Returns the smallest integer greater than or equal to a number.
+select ceil(4.3);  -- Result: 5
+select ceiling(-4.7);  -- Result: -4
+select ceil(4.1);
+-- Returns the largest integer less than or equal to a number.
+select floor(4.9);
+select floor(-0.1);
+-- Returns the exponential value of a number.
+select exp(1);
