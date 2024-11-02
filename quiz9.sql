45b6bd83dfbd4eb54a33746565529f770a3f98b7
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-02 10:05:17 -0700

    Queries With Solution

--- quiz9.sql
+++ quiz9.sql
@@ -1,12 +1,10 @@
--- 16 List the emp who are working for the Deptno 10 or 20.
-SELECT * FROM EMP WHERE EMP.DEPTNO IN (10,20);
--- 17 List the emps who are joined in the year 81
-SELECT * FROM EMP 
-WHERE EMP.HIREDATE >= '1981-01-01'
-AND EMP.HIREDATE <= '1981-12-31';
- -- 18 List the emps who are joined in the month of Aug 1980
-SELECT * FROM EMP WHERE EXTRACT(MONTH FROM EMP.HIREDATE) = 8;
- -- 19 List the emps Who Annual sal ranging from 22000 and 45000.
-SELECT * FROM EMP WHERE (EMP.SAL * 12)    BETWEEN 22000 AND 45000;
--- 20 List the Enames those are having five characters in their Names
-SELECT EMP.ENAME FROM EMP WHERE EMP.ENAME LIKE '_____';
+-- 16. List the emp who are working for the Deptno 10 or 20.
+select * from emp where emp.deptno = 10 or emp.deptno = 20; 
+-- 17. List the emps who are joined in the year 81. 
+select * from emp where extract(year from emp.hiredate) = 1981;
+-- 18. List the emps who are joined in the month of Aug 1980. 
+    select * from emp where emp.hiredate between '1981-08-01' and '1981-08-31';
+-- 19. List the emps Who Annual sal ranging from 22000 and 45000.
+    select * from emp where emp.sal * 12 between 22000 and 45000;
+-- 20. List the Enames those are having five characters in their Names.
+    select * from emp where emp.ename like '_____';
