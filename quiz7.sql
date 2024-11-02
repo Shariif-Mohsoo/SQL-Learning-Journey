b9884e5c51b88b255bfe352db547919e74f19bf3
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-02 10:04:38 -0700

    Queries With Solution

--- quiz7.sql
+++ quiz7.sql
@@ -1,18 +1,28 @@
--- 6 Display all the details of all ‘Mgrs’
-    SELECT * FROM EMP WHERE EMP.EMPNO IN (
-        SELECT DISTINCT EMP.MGR FROM EMP
-    );
--- 7 List the emps who joined before 1981
-    SELECT * FROM EMP WHERE EMP.HIREDATE <'1981-01-01';
--- 8 List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal
-    SELECT EMP.EMPNO, EMP.ENAME,EMP.SAL,
-    (EMP.SAL / 30) AS DAILY_SAL 
-    FROM EMP ORDER BY (EMP.SAL * 12) ASC;
--- 9 Display the Empno, Ename, job, Hiredate of all Mgrs
-    SELECT EMP.EMPNO,EMP.ENAME,EMP.JOB,EMP.HIREDATE FROM 
-    EMP WHERE EMP.MGR IN (
-        SELECT DISTINCT EMP.MGR FROM EMP
-    );
--- 10 List the Empno, Ename, Sal of all emps working for Mgr 7369. 
-SELECT EMP.EMPNO,EMP.ENAME,EMP.SAL FROM EMP WHERE EMP.MGR = 7369;
+--  5 queries
+
+--6. Display all the details of all ‘Mgrs’ 
+select * from emp 
+where emp.empno in 
+(
+    select mgr from emp
+);
+--7. List the emps who joined before 1981. 
+select * from emp
+where emp.hiredate <  to_date('01-01-1981', 'DD-MM-YYYY');;
+
+--8. List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal. 
+select emp.empno,emp.ename,emp.sal,(emp.sal / 30) as daily_sal from emp
+order by emp.sal * 12;
+
+--9. Display the Empno, Ename, job, Hiredate, Exp of all Mgrs 
+select emp.empno,emp.ename,emp.job,emp.hiredate,
+age(CURRENT_DATE,emp.hiredate) as exp
+from emp where emp.empno in (
+    select mgr from emp where mgr is not null
+);
+
+--10. List the Empno, Ename, Sal, Exp of all emps working for Mgr 7369.
+select emp.empno,emp.ename,emp.sal,
+age(CURRENT_DATE,emp.hiredate) as exp
+from emp where emp.mgr = 7369;
 
