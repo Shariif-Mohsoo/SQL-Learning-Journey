c38836cf43809685d06f6f7ff603ae42a174ecdc
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-02 10:05:06 -0700

    Queries With Solution

--- quiz8.sql
+++ quiz8.sql
@@ -1,19 +1,20 @@
--- 11 Display all the details of the emps whose Comm. Is more than their Sal
-SELECT * FROM EMP WHERE EMP.COMM > EMP.SAL;
--- 12 List the emps in the asc order of Designations of those joined 
--- after the second half of 1981(July 1, 1981)
-SELECT * FROM EMP WHERE EMP.HIREDATE > '1981-07-01' ORDER BY EMP.JOB;
--- 13 List the emps Daily Sal is more than Rs.100.
-SELECT * FROM EMP WHERE (EMP.SAL/ 30) > 100;
--- 14 List the emps who are either ‘CLERK’ or ‘ANALYST’ in the Desc order.
-SELECT * FROM EMP WHERE EMP.JOB IN ('CLERK','ANALYST') ORDER BY EMP.JOB DESC;
+--  5 queries
+-- 11. Display all the details of the emps whose Comm. Is more than their Sal. 
+    select * from emp where emp.comm > emp.sal;
+-- 12. List the emps in the asc order of Designations of those joined after the second half of 1981. 
+    select * from emp 
+    where emp.hiredate >= '1981-06-01'
+    order by job asc ;
 
--- 15 List the emps who joined on 1-MAY-81,3-DEC-81,17-DEC-81,19-JAN-80 in asc order of
--- seniority. 
-SELECT * FROM EMP WHERE EMP.HIREDATE IN (
-    '1-MAY-81',
-    '3-DEC-81',
-    '17-DEC-81',
-    '19-JAN-80'
-)
-ORDER BY EMP.HIREDATE ASC;
+-- 13. List the emps along with their Exp and Daily Sal is more than Rs.100. 
+    select emp.empno,emp.ename,emp.sal,
+    age(current_date,emp.hiredate) as exp
+    from emp where (emp.sal / 30) >= 100;
+
+-- 14. List the emps who are either ‘CLERK’ or ‘ANALYST’ in the Desc order. 
+    select * from emp where emp.job = 'CLERK' or emp.job = 'ANALYST';
+-- 15. List the emps who joined on 1-MAY-81,3-DEC-81,17-DEC-81,19-JAN-80 in asc order of seniority. 
+    select * from emp where emp.hiredate in 
+    ('1-MAY-81','3-DEC-81','17-DEC-81','19-JAN-80') 
+    order by age(current_date,emp.hiredate) asc;
+    
