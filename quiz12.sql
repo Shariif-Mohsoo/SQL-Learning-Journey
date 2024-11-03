a0e16c3659c5ab9b7c4280d479b43eb972054376
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-03 10:07:13 -0800

    Queries With Solution

--- quiz12.sql
+++ quiz12.sql
@@ -1,10 +1,14 @@
--- List all the emps who joined before or after 1981.
- select * from emp where extract(year from hiredate) <> 1981;
--- . List the emps whose Empno not starting with digit 78.
- select * from emp where cast(emp.empno as varchar) not like '78%';
--- List the emps who are working under ‘MGR’
-select * from emp where emp.mgr is not null;
--- List the emps who joined in any year but not belongs to the month of March.
-select * from emp where extract(month from hiredate) <> 5;
--- List the CLERK of Deptno 20
-select * from emp where emp.job = 'CLERK' and emp.deptno = 20;
+-- 31. List all the emps who joined before or after 1981. 
+    select * from emp where 
+    EXTRACT(year from hiredate) <= 1981
+    or 
+    EXTRACT(year from hiredate) > 1981;
+
+-- 32. List the emps whose Empno not starting with digit 78.
+    select * from emp where cast(emp.empno as varchar) not like '78%';
+-- 33. List the emps who are working under ‘MGR’. 
+    select * from emp where emp.mgr is not null;
+-- 34. List the emps who joined in any year but not belongs to the month of March.
+    select * from emp where to_char(emp.hiredate,'month') <> 'March';
+-- 35. List all the Clerks of Deptno 20. 
+    select * from emp where emp.job = 'CLERK' and emp.deptno = 20;
