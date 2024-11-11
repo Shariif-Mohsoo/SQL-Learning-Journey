39669996b9b6dd96781a1787b509af58e3a8fa4b
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-10 17:56:34 -0800

    Update Queries with solution

--- quiz16.sql
+++ quiz16.sql
@@ -1,6 +1,7 @@
 -- Qno1:
 -- Update job to 'TYPIST' where employee name is FORD?
 UPDATE emp SET job = 'TYPIST' WHERE ename = 'FORD';
+
 -- SELECT * from EMP Where emp.ename = 'FORD';
 
 -- Qno2: 
@@ -9,16 +10,21 @@ SELECT * from emp;
 UPDATE emp SET sal = sal + 1000;
 SELECT * from emp;
 
+
+
 -- Qno3: 
 -- Update 500 commission where job is salesman?
 SELECT * FROM emp WHERE job = 'SALESMAN';
 UPDATE emp SET comm = 500 WHERE job = 'SALESMAN';
 SELECT * FROM emp WHERE job = 'SALESMAN';
+
 -- Qno4: 
 -- Increase salary of employees upto 400 that name start from letter A?
 SELECT * FROM emp WHERE ename like 'A%';
 UPDATE emp SET sal = sal + 400 WHERE ename like 'A%';
 SELECT * FROM emp WHERE ename like 'A%';
+
+    
 -- Qno 5
 -- update salary of employee where emp id is 7900 and increament 1000?
 UPDATE emp SET sal = sal + 1000 WHERE empno = 7900;
