1abb0842e760100fb0ddf3045937eb79450325ef
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-10 17:57:25 -0800

    Number Functions

--- quiz20.sql
+++ quiz20.sql
@@ -1,10 +1,13 @@
 --THE BELOW WRITTEN ARE  NUMBER FUNCTIONS IN SQL
 -- to display the absolute value.
 select abs(-1);
+select abs(10);
 -- to display the reminder
 select mod(10,3);
+select mod(10,2);
 -- to display rounded value
 select round(22.22226,0);
+select round(22.46226,1);
 -- The TRUNC() function truncates a number to the specified number of decimal places without rounding.
 select trunc(12.262,2);
 -- to natural log of number
