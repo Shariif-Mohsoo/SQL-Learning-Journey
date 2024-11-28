c68fc2ce9b269016c5d4ec34bc3b5b3002e5b4cd
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-27 18:04:29 -0800

    Queries

--- quiz61.sql
+++ quiz61.sql
@@ -1,5 +1,8 @@
+select * from emp;
+select * from dept;
+
 -- Display the department number, last name, and job ID for every employee 
--- in the Executive  department.
+-- in the Sales  department.
 
 -- Display the employee numbers, last names, and salaries of all  employees who earn more 
 -- than the average salary and who work in a department with any  employee with a u in their name.
