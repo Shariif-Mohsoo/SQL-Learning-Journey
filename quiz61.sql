496d59a88be787087c05892b300183a72cb1892a
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-28 18:07:22 -0800

    Queries

--- quiz61.sql
+++ quiz61.sql
@@ -1,8 +1,17 @@
 select * from emp;
 select * from dept;
 
--- Display the department number, last name, and job ID for every employee 
+-- Display the department number, employee name, and job for every employee 
 -- in the Sales  department.
+select emp.deptno,
+    emp.ename,
+    emp.job
+from emp
+where emp.deptno = (
+        select dept.deptno
+        from dept
+        where dept.dname = 'SALES'
+    );
 
 -- Display the employee numbers, last names, and salaries of all  employees who earn more 
 -- than the average salary and who work in a department with any  employee with a u in their name.
