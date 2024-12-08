5e230a7234d5c68de34e9003c6545393d66c425d
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-08 07:24:40 +0500

    Queries With Solution

--- quiz58.sql
+++ quiz58.sql
@@ -1,6 +1,18 @@
+-- Write a query to display all the department names along with number of employees working in.
+-- GIVEN TABLE.
 create table employee (
     eid int primary key,
     ename varchar(30),
     dept varchar(30),
     salary int
 );
+-- insertion
+insert into employee
+values (1, 'Hashir', 'HR', 1000),
+    (2, 'Hamid', 'MRKT', 1000),
+    (3, 'Hashim', 'HR', 1000),
+    (4, 'Humais', 'MRKT', 1000),
+    (5, 'Huri Bahi', 'IT', 1000);
+-- select
+select *
+from employee;
