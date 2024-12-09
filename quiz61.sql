b9f4d243fd8a683cf0a2db9116419e54b831f289
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-09 14:31:12 -0800

    Queries

---
+++ quiz61.sql
@@ -0,0 +1,5 @@
+-- Display the department number, last name, and job ID for every employee 
+-- in the Executive  department.
+
+-- Display the employee numbers, last names, and salaries of all  employees who earn more 
+-- than the average salary and who work in a department with any  employee with a u in their name.
