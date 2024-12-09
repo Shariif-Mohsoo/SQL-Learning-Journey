24c9e9aefcd2be7552893b4874fd7e48c6d47bb1
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-09 14:16:46 -0800

    Queries with solution

--- quiz59.sql
+++ quiz59.sql
@@ -6,3 +6,14 @@ where employee.salary in(
         from employee
         group by dept
     );
+
+-- Create a query to display the empno and ename of all employees who earn more than the  average salary. 
+-- Sort the results in ascending order of salary
+select empno,
+    ename
+from emp
+where emp.sal > (
+        select avg(emp.sal)
+        from emp
+    )
+order by emp.sal;
