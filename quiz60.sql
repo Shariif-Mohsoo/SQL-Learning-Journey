cb560f1a7e10eb5de4ffb8a38cc545ac87a6ef3b
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-09 14:25:59 -0800

    Queries with solution

--- quiz60.sql
+++ quiz60.sql
@@ -1,2 +1,16 @@
 -- Write a query that displays the empno and ename of all employees 
--- who work in a  department with any employee whose last name contains a u.
+-- who work in a  department with any employee whose name contains a U.
+select * from emp;
+
+select e2.empno,
+    e2.ename
+from emp e2
+where e2.deptno in (
+        select distinct e1.deptno
+        from emp e1
+        where e1.ename like '%U%'
+    );
+
+-- Display the last name and salary of every employee who reports to King.
+
+
