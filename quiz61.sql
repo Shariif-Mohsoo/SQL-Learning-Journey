0be4b9ee2fe8473efb5eaa5caf0e4e9266873702
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-09 18:22:08 -0800

    Queries with solution

--- quiz61.sql
+++ quiz61.sql
@@ -15,13 +15,15 @@ where emp.deptno = (
 
 -- Display the employee numbers, names, and salaries of all  employees who earn more 
 -- than the average salary and who work in a department with any  employee with a U in their name.
-select *
-from emp
-where emp.sal > (
+select e3.empno,
+    e3.ename,
+    e3.sal
+from emp e3
+where e3.sal > (
         select round(avg(e2.sal), 2)
         from emp e2
     )
-    and emp.deptno = (
+    and e3.deptno = (
         select e1.deptno
         from emp e1
         where e1.ename like '%U%'
