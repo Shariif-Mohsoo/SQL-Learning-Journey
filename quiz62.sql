416282a7ccf112ffa0b56ac83b0d71e093993238
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-10 17:33:07 -0800

    Sub_Query

--- quiz62.sql
+++ quiz62.sql
@@ -1,6 +1,7 @@
 -- Define Sub Query?
 -- A query nested with in another query with parenthesis is called sub query.
 -- like 
+-- Display detail of all employees who salary is more than SMITH salary.
 select *
 from emp
 where emp.sal > (
@@ -17,6 +18,7 @@ where emp.sal > (
 -- 1.Scalar Subquery
 -- It returns only  1 row and 1 column.
 -- example:
+-- Display the name of all employees who salary is more than the avg salary of all employees.
 select e1.ename
 from emp e1
 where e1.sal > (
@@ -34,6 +36,8 @@ from emp e1
 
 -- 2. Multiple row subquery
 --a. query that returns multiple rows and multiple columns
+-- example:
+-- Display the detail of all emp who salary is highest in the department.
 select emp.*
 from emp
 where (emp.sal, emp.deptno) in (
@@ -43,9 +47,24 @@ where (emp.sal, emp.deptno) in (
         group by deptno
     );
 --b. query that returns 1 cols but multiple rows.
+-- example:
+-- Display the detail of those departments who are not assign any employee.
 select *
 from dept
 where deptno not in (
         select distinct deptno
         from emp
+    );
+
+-- 3. Correlated Subquery
+-- Query which is related to the outer query.
+-- The processing of the subquery depends on the values return by outer query.
+-- example
+-- Find the employees in each department who earn more than the average salary in that department
+select *
+from emp e1
+where e1.sal > (
+        select round(avg(e2.sal))
+        from emp e2
+        where e2.deptno = e1.deptno
     );
