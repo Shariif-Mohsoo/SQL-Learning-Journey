520e05e1453c7230ce017268952ab0fb495b46e7
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-12 12:13:36 -0800

    Sub_Query

--- quiz64.sql
+++ quiz64.sql
@@ -1,1 +1,13 @@
--- 
+--TODO: Subquery using Select clause.
+-- FETCH ALL THE EMPLOYEE DETAILS AND ADD THE REMARK COLUMN TO THOSE EMPLOYEE WHO EARN MORE THAN THE AVG SALARY OF ALL EMPLOYEES.
+select *,
+    (
+        case
+            when emp.sal > (
+                select avg(emp.sal)
+                from emp
+            ) then 'Higher than average salary'
+            else null
+        end
+    ) as remarks
+from emp;
