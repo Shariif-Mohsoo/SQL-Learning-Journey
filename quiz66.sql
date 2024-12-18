78a4c2ad698983f9ddf1c3125d8d604145788724
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-18 08:14:16 +0500

    Queries with solution

--- quiz66.sql
+++ quiz66.sql
@@ -3,6 +3,7 @@ from employee;
 -- list the employees who sal is equal to avg sal.
 select *
 from employee
-where salary =
-select round(avg(salary), 0)
-from employee;
+where salary = (
+        select round(avg(salary), 0)
+        from employee
+    );
