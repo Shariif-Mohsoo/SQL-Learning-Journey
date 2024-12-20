5238ad2cc2fc14282819a08c74ebfbf912425830
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-19 19:07:19 -0800

    Query with solution

--- quiz68.sql
+++ quiz68.sql
@@ -1,3 +1,9 @@
 -- FIND ALL EMPLOYEES DETAIL WHO WORK IN A DEPARTMENT.
 -- WITH SCALAR SUB QUERY
+select *
+from emp
+where emp.deptno in (
+        select deptno
+        from dept
+    );
 -- WITH CO_RELATED SUB QUERY
