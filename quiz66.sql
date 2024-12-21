ffaa18577949c5b66b7a6f903887a2b632a912d8
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-21 05:28:17 +0500

    Queries with solution

--- quiz66.sql
+++ quiz66.sql
@@ -7,3 +7,15 @@ where salary = (
         select round(avg(salary), 0)
         from employee
     );
+select *
+from emp;
+-- Display the department name and employee name for those employees who manager is BLAKE.
+select dept.dname,
+    emp.ename
+from dept
+    join emp on emp.deptno = emp.deptno
+where emp.mgr in (
+        select emp.empno
+        from emp
+        where emp.ename = 'BLAKE'
+    );
