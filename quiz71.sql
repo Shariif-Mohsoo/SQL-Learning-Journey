a025d738fdbe2d65cba76474e64d92d68927a677
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-22 06:48:17 +0500

    Queries with solution

---
+++ quiz71.sql
@@ -0,0 +1,6 @@
+-- Write a query to display employee name , job of all employees who are 
+-- working as manager and works at CHICAGO.
+-- List the employees who have commission greater than the maximum salary of all the SALESMAN
+-- who don't report to KING directly.
+-- Display the names of employees from department number 10 with salary greater than that
+-- of all employees working in other departments.
