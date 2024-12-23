d921d1e16c1f915fcc33cb80d7c8024a8feacf85
Shariif-Rajpoot <mr8781256@gmail.com>
2024-12-23 07:31:20 +0500

    Queries with solution

--- quiz73.sql
+++ quiz73.sql
@@ -1,2 +1,13 @@
 -- LIST THE EMPLOYEES WHO JOINED AFTER 2 YEARS 
 -- OF FIRST EMPLOYEE OF THE COMPANY AND MORE THAN BLAKE SALARY.
+select *
+from emp
+where emp.hiredata > (
+        select min(hiredata) +(365 * 2)
+        from emp
+    )
+    and emp.sal > (
+        select sal
+        from emp
+        where emp.ename = 'BLAKE'
+    );
