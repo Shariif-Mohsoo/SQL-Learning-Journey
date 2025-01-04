be2790ec80b7c8aa478a06b495e41c990b4a9ef3
Shariif-Rajpoot <mr8781256@gmail.com>
2025-01-04 20:15:25 +0500

    SQL View

--- quiz76.sql
+++ quiz76.sql
@@ -3,5 +3,7 @@
 -- view is noting just 
 -- 1) Virtual Table((WHY?) (Reason:It's look like a table but not actual table. Not acquire any space))
 -- 2) View is the result set of a stored query.
--- 3)
+-- 3) View can be created in two ways:
+--      a) Read-only(They should be only read able not modify able.)
+--      b) Update able
 -- 4)
