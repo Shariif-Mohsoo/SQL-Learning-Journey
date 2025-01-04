6ea12740a5ddfaf433e0900447b0d4a059e68340
Shariif-Rajpoot <mr8781256@gmail.com>
2025-01-04 20:18:02 +0500

    SQL View

--- quiz76.sql
+++ quiz76.sql
@@ -5,5 +5,5 @@
 -- 2) View is the result set of a stored query.
 -- 3) View can be created in two ways:
 --      a) Read-only(They should be only read able not modify able.)
---      b) Update able
+--      b) Update able(Changes in the view will also be reflected in the base table)
 -- 4)
