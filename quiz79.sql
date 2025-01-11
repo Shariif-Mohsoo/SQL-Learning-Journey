0de568b4d92a9dec5ae1274c96b0daa38b53d24e
Shariif-Mohsoo <mr8781256@gmail.com>
2025-01-11 10:42:19 -0800

    Procedures

--- quiz79.sql
+++ quiz79.sql
@@ -1,4 +1,13 @@
-select * from emp;
--- 1
-create procedure selectAll
-as select * from emp;
+CREATE OR REPLACE 
+PROCEDURE select_all()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    CREATE TEMP TABLE
+    temp_emp AS
+    SELECT * FROM emp;
+END;
+$$;
+
+CALL select_all();
+SELECT * FROM temp_emp;
