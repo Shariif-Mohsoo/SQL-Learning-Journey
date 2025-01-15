b8f50d6996eb0ac98cf40f8b6e21ab65da8c6e2a
Shariif-Mohsoo <mr8781256@gmail.com>
2025-01-14 16:52:52 -0800

    SQL

--- updatingRecordsInTable.sql
+++ updatingRecordsInTable.sql
@@ -8,5 +8,4 @@ update students set name = 'Ali' where stdId = 1 and major = 'Hidden';
 update students set major = 'Biology' where stdId = 1 or major = 'Hidden';
 update students set major = 'Bio' where major = 'Biology';
 SELECT * FROM students
-ORDER BY stdId ASC;
-select * FROM students;
+ORDER BY stdId DESC;
