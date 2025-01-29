649013277b8b0ec8fb4d015aebbe5faab908eeda
Shariif-Mohsoo <mr8781256@gmail.com>
2025-01-28 19:09:11 -0800

    Revision

--- revision.sql
+++ revision.sql
@@ -1,4 +1,5 @@
 -- DQL(Data Query Language)
--- SELECT state is used for query purpose;
+-- SELECT statement is used for query purpose;
 SELECT * FROM emp;
 -- DML(Data Manipulation Language)
+-- INSERT,UPDATE,DELETE STATEMENTS WILL BE USED
